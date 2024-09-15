a38b24dcf186d44098f586aa49080a338cba95e8
vsh1411 <vsh1411@gmail.com>
2024-09-15 07:22:36 +0000

    hero section setup

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,19 @@
+import "../styles/HeroSection.css"
+import restaurantImg from "../../public/og-image.jpg"
+
 function HeroSection() {
-    return <h1>Hero Section</h1>;
+    return (
+        <div id="main-container-hero">
+            <div id="content">
+                <h1>Little Lemon</h1>
+                <h3>Chicago</h3>
+                <p>Little Lemon is a vibrant and contemporary restaurant chain offering a fresh take on dining with a focus on zesty, flavorful dishes. Inspired by the lively essence of lemons, our menu blends bold flavors and healthy ingredients, bringing a refreshing twist to classic and modern cuisine.</p>
+            </div>
+            <div id="image">
+                <img src={restaurantImg} alt="Restaurant setting" />
+            </div>
+        </div>
+    );
 }
 
 export default HeroSection;
